1786534b0439c027586f87fd6f82f31866290d54
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-09-12 20:17:27 +0200

    reset

--- PTAP.Web/wwwroot/js/site.js
+++ PTAP.Web/wwwroot/js/site.js
@@ -2,11 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-import Typed from '~/lib/typed.js/typed.js';
-
-var options = {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-    typeSpeed: 40
-};
-
-var typed = new Typed('.element', options);
