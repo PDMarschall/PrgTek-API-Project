ad0b6d8e5621b8d021a8cbd92b2aae44fe0900a6
Peter Damgaard Marschall <15450@student.eadania.dk>
2022-09-12 19:11:54 +0200

    yes

--- PTAP.Web/wwwroot/js/site.js
+++ PTAP.Web/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+import Typed from 'typed.js';
+
+var options = {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
