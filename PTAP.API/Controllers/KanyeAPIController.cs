a8228e3c5f0432ea59ae6ed3c26ec89fd4cc31d2
Peter Marschall <PDMarschall@hotmail.com>
2022-09-16 10:12:55 +0200

    api oprettet

---
+++ PTAP.API/Controllers/KanyeAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTAP.API.Controllers
+{
+    public class KanyeAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
